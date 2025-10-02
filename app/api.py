2316337cb3e4d8e59ae960360615e2c74caf6351
ezeta03 <sergio.zenteno@globalfiber.com.pe>
2025-10-02 10:46:16 -0500

    ADD: deep-translate

--- app/api.py
+++ app/api.py
@@ -4,7 +4,7 @@ import os
 from uuid import uuid4
 from app.worker_task import translate_file_task
 from pydantic import BaseModel
-from translator import translate_text
+from translation_service import translate_text
 
 app = FastAPI()
 
